79ee9d10cc33c9bbfecb204758bbb07b796dc9cf
Máté Lajkó <mate.lajko@stritzinger.com>
2023-06-26 17:29:37 +0200

    Fix verify_peer as server
    The Msg value was invalid input for base64:encode/1.

--- src/braidnode_app.erl
+++ src/braidnode_app.erl
@@ -1,13 +1,12 @@
 %%%-------------------------------------------------------------------
-%% @doc gateway public API
+%% @doc Braidnode application module
 %% @end
 %%%-------------------------------------------------------------------
-
 -module(braidnode_app).
-
 -behaviour(application).
 
 -export([start/2, stop/1]).
+
 
 start(_StartType, _StartArgs) ->
     braidnode_sup:start_link().
--- src/braidnode_crypto.erl
+++ src/braidnode_crypto.erl
@@ -4,13 +4,13 @@
 
 sign_fun(_Version, Msg, HashAlg, _Key, SignAlg) ->
     Result = braidnode_client:send_receive(sign, #{
-        payload => base64:encode(Msg),
+        payload => base64:encode(erlang:term_to_binary(Msg)),
         hash_alg => HashAlg,
         sign_alg => SignAlg
     }),
     case Result of
         Signature when is_binary(Signature) ->
-            base64:decode(Result);
+            erlang:binary_to_term(base64:decode(Result));
         #{error := Reason} ->
             % The error will be catched by ssl and displayed as an alert
             error(Reason)

6bbbcf12dc8880274d27a6b3b31bdc4693b6f998
Máté Lajkó <mate.lajko@stritzinger.com>
2023-06-30 23:29:48 +0200

    Restore dummy plugin provider callbacks
    This is to avoid rebar3 warnings about the missing functions.

--- src/braidnode.erl
+++ src/braidnode.erl
@@ -6,6 +6,10 @@
 -export([whereis/1]).
 -export([send/2]).
 
+%% callbacks for the braidnode rebar3 plugin
+-export([init/1,
+         do/1,
+         format_error/1]).
 
 register(Name, Pid) ->
     pg:join(braidnet, Name, Pid).
@@ -19,3 +23,16 @@ whereis(Name) ->
 send(Name, Msg) ->
     Pids = pg:get_members(braidnet, Name),
     [P ! Msg || P <- Pids].
+
+%--- Callbacks for the braidnode rebar3 plugin ---------------------------------
+-spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
+init(State) ->
+    {ok, State}.
+
+-spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
+do(State) ->
+    {ok, State}.
+
+-spec format_error(any()) -> iolist().
+format_error(Reason) ->
+    io_lib:format("~p", [Reason]).
